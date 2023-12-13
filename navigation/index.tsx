fcc394881e3f4c8d80be7c061ad6be0df7853e33
jbdura <bidurajohnson@gmail.com>
2023-12-13 14:55:27 +0300

    Clean up navigation/index.tsx

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,10 +1,6 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './RootNav';
 
-
-
-// const Stack = createNativeStackNavigator();
 
 export default function Navigation() {
   return (
