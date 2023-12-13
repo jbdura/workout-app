e35bf4764b24d2df745bcd107e055155b4cd671b
jbdura <bidurajohnson@gmail.com>
2023-12-13 12:14:27 +0300

    First edit of HomeScreen and PlannerScreen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,8 +1,22 @@
-
+import { View, Text, StyleSheet } from "react-native";
 
 
 function HomeScreen() {
-  return (  );
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
 }
 
 export default HomeScreen;
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -1,8 +1,24 @@
 
+import { View, Text, StyleSheet } from "react-native";
 
 
 function PlannerScreen() {
-  return (  );
+  return (
+    <View style={styles.container}>
+      <Text>Planner Screen</Text>
+
+    </View>
+  );
 }
 
 export default PlannerScreen;
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
