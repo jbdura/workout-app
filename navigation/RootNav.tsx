344d5f7b9baa3c93de312374b871ae6418187fe5
jbdura <bidurajohnson@gmail.com>
2023-12-13 14:56:34 +0300

    Added Bottom navigation in the root navigation RootNav

--- navigation/RootNav.tsx
+++ navigation/RootNav.tsx
@@ -1,21 +1,18 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import { NavigationContainer } from '@react-navigation/native';
 
-import HomeScreen from "../screens/HomeScreen";
-import PlannerScreen from "../screens/PlannerScreen";
+// import HomeScreen from "../screens/HomeScreen";
+// import PlannerScreen from "../screens/PlannerScreen";
+import BottomTabNav from './bottomTab';
 
 const Stack = createNativeStackNavigator();
 
 function RootNavigator() {
   return (
-    <Stack.Navigator initialRouteName='Home'>
+    <Stack.Navigator>
       <Stack.Screen
-        name='Home'
-        component={HomeScreen}
-      />
-      <Stack.Screen
-        name='Planner'
-        component={PlannerScreen}
+        name='Workout App'
+        component={BottomTabNav}
       />
     </Stack.Navigator>
   );
