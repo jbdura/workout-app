edce6873f461edbf88f8304f99d63257d76f9b97
jbdura <bidurajohnson@gmail.com>
2023-12-13 14:11:47 +0300

    Created root navigator

---
+++ navigation/RootNav.tsx
@@ -0,0 +1,24 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import HomeScreen from "../screens/HomeScreen";
+import PlannerScreen from "../screens/PlannerScreen";
+
+const Stack = createNativeStackNavigator();
+
+function RootNavigator() {
+  return (
+    <Stack.Navigator initialRouteName='Home'>
+      <Stack.Screen
+        name='Home'
+        component={HomeScreen}
+      />
+      <Stack.Screen
+        name='Planner'
+        component={PlannerScreen}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default RootNavigator;
