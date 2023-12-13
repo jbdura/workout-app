36b11b4805a75b3bd9a605c619315591f6bc0e34
jbdura <bidurajohnson@gmail.com>
2023-12-13 14:55:59 +0300

    Created bottomTab feature and component

---
+++ navigation/bottomTab.tsx
@@ -0,0 +1,23 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/HomeScreen";
+import PlannerScreen from "../screens/PlannerScreen";
+
+
+const BottomTab = createBottomTabNavigator();
+
+function BottomTabNav() {
+  return (
+    <BottomTab.Navigator initialRouteName='Home'>
+      <BottomTab.Screen
+        name='Home'
+        component={HomeScreen}
+      />
+      <BottomTab.Screen
+        name='Planner'
+        component={PlannerScreen}
+      />
+    </BottomTab.Navigator>
+  );
+}
+
+export default BottomTabNav;
