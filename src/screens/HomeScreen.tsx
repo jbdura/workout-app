22f1cd138d276d99917f68d5646c0c37fb54fd81
jbdura <bidurajohnson@gmail.com>
2023-12-15 21:48:04 +0300

    Moved data rendering from button to variable

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import { useEffect } from "react";
+import data from '../data.json'
 
 
 function HomeScreen({ navigation }: NativeStackHeaderProps) {
@@ -10,13 +11,22 @@ function HomeScreen({ navigation }: NativeStackHeaderProps) {
 
     return () => console.log("Unmounting Home Screen")
   }, []);
-  
+
+
+  const renderItem = ({ item }:any) => (
+    <View>
+      <Text>{ item.name }</Text>
+    </View>
+  )
+
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <Button
-      title='Go to Planner'
-      onPress={() => navigation.navigate("Planner")}
+      {/* <Text>Home Screen</Text> */}
+      {/* <Text> { JSON.stringify(data) }</Text> */}
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.slug}
       />
     </View>
   );
@@ -28,6 +38,7 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
