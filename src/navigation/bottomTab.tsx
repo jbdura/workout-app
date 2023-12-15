11bef692752633cd8fcc4722e16f683c065b783b
jbdura <bidurajohnson@gmail.com>
2023-12-15 13:28:29 +0300

    Added icons to bottomTab

--- src/navigation/bottomTab.tsx
+++ src/navigation/bottomTab.tsx
@@ -1,21 +1,37 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from '@expo/vector-icons';
 import HomeScreen from "../screens/HomeScreen";
 import PlannerScreen from "../screens/PlannerScreen";
+import { Text } from "react-native";
 
 
 const BottomTab = createBottomTabNavigator();
 
 function BottomTabNav() {
   return (
-    
+
     <BottomTab.Navigator initialRouteName='Home'>
       <BottomTab.Screen
         name='Home'
         component={HomeScreen}
+        options={
+          {
+            unmountOnBlur: true,
+            tabBarIcon: ({color, size}) =>
+              <MaterialIcons name="home" size={size} color={color} />
+          }
+        }
       />
       <BottomTab.Screen
         name='Planner'
         component={PlannerScreen}
+        options={
+          {
+            unmountOnBlur: true,
+            tabBarIcon: ({color, size}) =>
+              <MaterialIcons name="add-task" size={size} color={color} />
+          }
+        }
       />
     </BottomTab.Navigator>
   );
