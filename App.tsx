3d97b4b1f63baa325028f4eab1a3282667a5f7a9
jbdura <bidurajohnson@gmail.com>
2023-12-18 11:45:44 +0300

    Imported useCacheResources to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import Navigation from './src/navigation';
 import { View } from 'react-native';
+import useCacheResources from './src/hooks/useCacheResources';
 
 export default function App() {
+
+  const isLoaded = useCacheResources();
+  
   return (
     <>
       <Navigation />
