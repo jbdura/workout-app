f175e4e80cbdc73541d2035dec2925c9262436bc
jbdura <bidurajohnson@gmail.com>
2023-12-18 12:27:22 +0300

    async function of the fonts

--- src/hooks/useCacheResources.ts
+++ src/hooks/useCacheResources.ts
@@ -1,13 +1,24 @@
 import { useState, useEffect } from "react";
+import * as Font from 'expo-font';
 
 export default function useCacheResources() {
-  const [isLoadingComplete, setIsLoadingComplete] = useState(false);
-  
+  const [ isLoadingComplete, setIsLoadingComplete ] = useState(false);
+
   useEffect(() => {
-    function loadResourcesAndDataAsync() {
-      setTimeout(() => {
-        setIsLoadingComplete(true) 
-      }, 3000)
+    async function loadResourcesAndDataAsync() {
+
+      try {
+        await  Font.loadAsync({
+          "montserrat": require("../../assets/fonts/Montserrat-Regular.ttf"),
+          "montserrat-bold": require("../../assets/fonts/Montserrat-Bold.ttf"),
+        })
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setIsLoadingComplete(true)
+      }
+
+
     }
 
     loadResourcesAndDataAsync();
